8a717e48ecb77ad0b718e1214758279ffa09758c
jgomez <jgomez@lurin>
2018-02-17 10:21:03 -0500

    first commit

---
+++ mod03_sesion06_Spring/src/main/java/com/tecsup/gestion/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.tecsup.gestion.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class EmployeeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+
+
+	@GetMapping("/admin/menu")
+	public String menu() {
+
+		return "/admin/menu";
+	}
+
+}
